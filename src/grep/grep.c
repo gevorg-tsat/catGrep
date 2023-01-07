71a16045ddac718a7f0a7564c3fd419e58086a87
gevorg-tsat <72250109+gevorg-tsat@users.noreply.github.com>
2023-01-07 17:29:20 +0300

    feat: grep parsing

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -1,1 +1,66 @@
 #include "grep.h"
+
+int parse(int argc, char** argv, flags* flags, int* file_id) {
+        int param_count = 0;
+    for (; param_count + 1 < argc && argv[param_count + 1][0] == '-'; param_count++);
+    const char* short_options = "eivclnhsfo";
+    const struct option long_options[] = {
+        {"line-number", no_argument, NULL, 'n'},
+        {"file", required_argument, NULL, 'f'},
+        {"no-filename", no_argument, NULL, 'h'},
+        {NULL, 0, NULL, 0},
+        {"count", no_argument, NULL, 'c'},
+        {"ignore-case", no_argument, NULL, 'i'},
+        {"files-with-matches", no_argument, NULL, 'f'},
+        {"only-matching", no_argument, NULL, 'o'},
+        {"no-messages", no_argument, NULL, 's'},
+        {"invert-match", no_argument, NULL, 'v'},
+    };
+    int rez, option_index, i = 0;
+    opterr = 0;
+    optind = 1;
+    while (i < param_count && (rez = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1) {
+        switch (rez) {
+            case 'e':
+                flags -> e = 1;
+                break;
+            case 'i':
+                flags -> i = 1;
+                break;
+            case 'v':
+                flags -> v = 1;
+                break;
+            case 'c':
+                flags -> c = 1;
+                break;
+            case 'l':
+                flags -> l = 1;
+                break;
+            case 'n':
+                flags -> n = 1;
+                break;
+            case 'h':
+                flags -> h = 1;
+                break;
+            case 's':
+                flags -> s = 1;
+                break;
+            case 'f':
+                flags -> f = 1;
+                break;
+            case 'o':
+                flags -> o = 1;
+                break;
+            default:
+                fprintf(stderr, "Wrong param");
+                return 1;
+        }
+        i++;
+    }
+    *file_id = optind;
+    return 0;
+}
+
+void grep(char* filename, char* find, flags flags) {
+    
+}
--- src/grep/grep.h
+++ src/grep/grep.h
@@ -1,3 +1,19 @@
 #include <stdio.h>
 #include <regex.h>
+#include <getopt.h>
 
+typedef struct flags {
+    int e = 0;
+    int i = 0;
+    int v = 0;
+    int c = 0;
+    int l = 0;
+    int n = 0;
+    int h = 0;
+    int s = 0;
+    int f = 0;
+    int o = 0;
+} flags;
+
+int parse(int argc, char** argv, flags* flags, int* file_id);
+void grep(char* filename, char* find, flags flags);
