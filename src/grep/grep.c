1addc17cc657f1ba84123fcc3373693dd9b13534
Knot Hulk <knothulk@mi-b2.msk.21-school.ru>
2023-01-09 18:20:14 +0300

    feat: grep done, testing left

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -3,10 +3,15 @@
 int main(int argc, char* argv[]) {
     flags flags = {0};
     int file_id = 0;
-    if (parse(argc, argv, &flags, &file_id))
+    char pattern[LINEMAX];
+    char pattern_file[LINEMAX * _LINE_AMOUNT_MAX_];
+    if (parse(argc, argv, &flags, &file_id, pattern, pattern_file))
         return 1;
-    for (int i = file_id; i < argc; i++)
-        grep(
+    for (int i = file_id; i < argc; i++) {
+        grep(argv[file_id], pattern, flags);
+        if (flags.f)
+            grep(argv[file_id], pattern_file, flags);
+    }
     return 0;
 }
 
@@ -45,9 +50,23 @@ int count(node* head) {
     return cnt;
 }
 
-int parse(int argc, char** argv, flags* flags, int* file_id, char pattern[LINEMAX], char pattern_file[LINEMAX]) {
-    int param_count = 0;
-    for (; param_count + 1 < argc && argv[param_count + 1][0] == '-'; param_count++);
+int read_pattern_file(char* filename, char pattern[LINEMAX * _LINE_AMOUNT_MAX_]) {
+    char line[LINEMAX];
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, "grep: %s: No such file or directory", filename);
+        return 1;
+    }
+    while(!feof(file)) {
+        fgets(line, LINEMAX, file);
+        strcat(pattern, line);
+        strcat(pattern, "|");
+    }
+    fclose(file);
+    return 0;
+}
+
+int parse(int argc, char** argv, flags* flags, int* file_id, char pattern[LINEMAX], char pattern_file[LINEMAX * _LINE_AMOUNT_MAX_]) {
     const char* short_options = "e:ivclnhsf:o";
     const struct option long_options[] = {{NULL, 0, NULL, 0}};
     int rez, option_index, i = 0;
@@ -55,7 +74,7 @@ int parse(int argc, char** argv, flags* flags, int* file_id, char pattern[LINEMA
     optind = 1;
     pattern[0]='\0';
     pattern_file[0]='\0';
-    while (i < param_count && (rez = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1) {
+    while ((rez = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1) {
         switch (rez) {
             case 'e':
                 flags -> e = 1;
@@ -86,8 +105,7 @@ int parse(int argc, char** argv, flags* flags, int* file_id, char pattern[LINEMA
                 break;
             case 'f':
                 flags -> f = 1;
-                strcat(pattern_file, optarg);
-                strcat(pattern_file, "|");
+                read_pattern_file(optarg, pattern_file);
                 break;
             case 'o':
                 if (!(flags->v))
@@ -104,24 +122,6 @@ int parse(int argc, char** argv, flags* flags, int* file_id, char pattern[LINEMA
         optind++;
     }
     *file_id = optind;
-    return 0;
-}
-
-int read_pattern_file(char* filename, char pattern[LINEMAX * _LINE_AMOUNT_MAX_]) {
-    pattern[0] = '\0';
-    char line[LINEMAX];
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        //if (!(flags.s))
-            //fprintf(stderr, "grep: %s: No such file or directory", filename);
-        return 1;
-    }
-    while(!feof(file)) {
-        fgets(line, LINEMAX, file);
-        strcat(pattern, line);
-        strcat(pattern, "|");
-    }
-    fclose(file);
     return 0;
 }
 
@@ -146,7 +146,7 @@ void grep(char* filename, char* pattern, flags flags) {
     while(!feof(file)) {
         fgets(line, LINEMAX, file);
         int not_found = regexec(&regex, line, 1, &match, 0);
-        if ((!not_found && !flags.v) || (not_found && flags.v))
+        if ((!not_found && !flags.v) || (not_found && flags.v)) {
             if (!flags.o || flags.v)
                 head = add(head, line_counter, line);
             else {
@@ -154,13 +154,15 @@ void grep(char* filename, char* pattern, flags flags) {
                     char mat[LINEMAX];
                     int i = 0;
                     while (i != match.rm_eo - match.rm_so) {
-                        mat[i] = line[match.rm_so + i++];
+                        mat[i] = line[match.rm_so + i];
+                        i++;
                     }
                     mat[i] = '\0';
                     head = add(head, line_counter, mat);
                     not_found = regexec(&regex, line + match.rm_eo, 1, &match, 0);
                 }
             }
+        }
         line_counter++;
     }
     // out
