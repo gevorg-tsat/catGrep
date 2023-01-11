32e6296ca26e3eedd09ca78ca7660644a05e6423
gevorg-tsat <72250109+gevorg-tsat@users.noreply.github.com>
2023-01-11 22:44:09 +0300

    trying to format

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -4,17 +4,17 @@ void cat(char* filename, int flags[6]) {
     FILE *file = fopen(filename, "r");
     if (!file) {
         fprintf(stderr, "cat: %s: No such file or directory", filename);
-        //fclose(file);
+        // fclose(file);
         return;
     }
     int c = 0;
-    //flags:
+    // flags:
     // [0] -b: non empty
     // [1] -e: $ instead of \n
     // [2] -n: all lines
     // [3] -s squeeze empty lines
-    // [4] -t ^I instead of tab 
-    
+    // [4] -t ^I instead of tab
+
     int empty_test = 0, empty_flag = 0;
     c = fgetc(file);
     int lines_counter = 0;
@@ -28,14 +28,14 @@ void cat(char* filename, int flags[6]) {
         if (flags[3] && empty_flag && c == '\n')
             {;}
         else if (flags[2] && empty_test)
-            printf("%6d%c", ++lines_counter,9);
-        else if (flags[0] && empty_test && c!='\n')
-            printf("%6d%c", ++lines_counter,9);
+            printf("%6d%c", ++lines_counter, 9);
+        else if (flags[0] && empty_test && c != '\n')
+            printf("%6d%c", ++lines_counter, 9);
         else if (flags[0] && empty_test && flags[1] && c == '\n')
             printf("      %c", 9);
-        if (flags[5] && (c >=0 && c <=31) && c!=10 && c!=9)
+        if (flags[5] && (c >= 0 && c <= 31) && c != 10 && c != 9)
             printf("^%c", c + 64);
-        else if (flags[5] && (c>=127))
+        else if (flags[5] && (c >= 127))
             printf("^%c", c - 64);
         else if (flags[1] && !(flags[3] && empty_flag) && c == '\n')
             printf("$%c", c);
@@ -47,15 +47,16 @@ void cat(char* filename, int flags[6]) {
             empty_test = 1;
         } else if (c == '\n' && empty_test) {
             empty_flag = 1;
-        } else
+        } else {
             empty_test = 0, empty_flag = 0;
+        }
         c = fgetc(file);
     }
     fclose(file);
 }
 
 int parse(int argc, char** argv, int flags[6], int* file_id) {
-    //flags:
+    // flags:
     // [0] -b: non empty
     // [1] -E: $ instead of \n
     // [2] -n: all lines
@@ -63,7 +64,7 @@ int parse(int argc, char** argv, int flags[6], int* file_id) {
     // [4] -T ^I instead of tab
     // [5] -v
     int param_count = 0;
-    for (; param_count + 1 < argc && argv[param_count + 1][0] == '-'; param_count++);
+    for (; param_count + 1 < argc && argv[param_count + 1][0] == '-'; param_count++) {}
     const char* short_options = "beEnstTv";
     const struct option long_options[] = {
         {"number-nonblank", no_argument, NULL, 'b'},
@@ -74,7 +75,8 @@ int parse(int argc, char** argv, int flags[6], int* file_id) {
     int rez, option_index, i = 0;
     opterr = 0;
     optind = 1;
-    while (i < param_count && (rez = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1) {
+    while (i < param_count &&
+            (rez = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1) {
         switch (rez) {
             case 'b':
                 flags[0] = 1;
@@ -115,9 +117,9 @@ int parse(int argc, char** argv, int flags[6], int* file_id) {
 }
 
 int main(int argc, char* argv[]) {
-    int flags[] = {0,0,0,0,0,0};
+    int flags[] = {0, 0, 0, 0, 0, 0};
     int file_id = 0;
-    if (parse(argc, argv,flags, &file_id))
+    if (parse(argc, argv, flags, &file_id))
         return 1;
     for (int i = file_id; i < argc; i++)
         cat(argv[i], flags);
